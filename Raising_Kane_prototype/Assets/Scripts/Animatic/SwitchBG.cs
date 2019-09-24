11f15fb7aeb7796e7f94ffbfe0b09e2187cbb9f2
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2019-09-24 00:52:30 -0600

    animatic scene created and changed original boss sprite

---
+++ Raising_Kane_prototype/Assets/Scripts/Animatic/SwitchBG.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class SwitchBG : MonoBehaviour
+{
+
+    public SpriteRenderer background;
+    public Sprite oneSheet;
+    public Sprite Anim1;
+    public Sprite Anim2;
+    public Sprite Anim3;
+    public Sprite Anim4;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background.sprite = oneSheet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            background.sprite = Anim1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            background.sprite = Anim2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            background.sprite = Anim3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            background.sprite = Anim4;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            background.sprite = oneSheet;
+        }
+
+    }
+           
+}
